05085baeca98d07886d7ffb6eb406d569c2e6233
Tanim Omor <tanim.pro@gmail.com>
2024-07-15 23:54:21 +0600

    Complete Initial Release!

---
+++ data/index.ts
@@ -0,0 +1,208 @@
+export const navItems = [
+    {name: "About", link: "#about"},
+    {name: "Projects", link: "#projects"},
+    {name: "Testimonials", link: "#testimonials"},
+    {name: "Contact", link: "#contact"},
+];
+
+export const gridItems = [
+    {
+        id: 1,
+        title: "I prioritize client collaboration, fostering open communication ",
+        description: "",
+        className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+        imgClassName: "w-full h-full",
+        titleClassName: "justify-end",
+        img: "/b1.svg",
+        spareImg: "",
+    },
+    {
+        id: 2,
+        title: "I'm very flexible with time zone communications",
+        description: "",
+        className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+        imgClassName: "",
+        titleClassName: "justify-start",
+        img: "",
+        spareImg: "",
+    },
+    {
+        id: 3,
+        title: "My tech stack",
+        description: "I constantly try to improve",
+        className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+        imgClassName: "",
+        titleClassName: "justify-center",
+        img: "",
+        spareImg: "",
+    },
+    {
+        id: 4,
+        title: "Tech enthusiast with a passion for development.",
+        description: "",
+        className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+        imgClassName: "",
+        titleClassName: "justify-start",
+        img: "/grid.svg",
+        spareImg: "/b4.svg",
+    },
+
+    {
+        id: 5,
+        title: "Currently building a Stock Market website.",
+        description: "The Inside Scoop",
+        className: "md:col-span-3 md:row-span-2",
+        imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+        titleClassName: "justify-center md:justify-start lg:justify-center",
+        img: "/b5.svg",
+        spareImg: "/grid.svg",
+    },
+    {
+        id: 6,
+        title: "Do you want to start a project together?",
+        description: "",
+        className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+        imgClassName: "",
+        titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+        img: "",
+        spareImg: "",
+    },
+];
+
+export const projects = [
+    {
+        id: 1,
+        title: "Quicket - Ticketing Platform for Events",
+        des: "At Coresoft | Discover a hassle-free way to explore and purchase tickets for a variety of events with Quket. Whether it's the pulsating beats of concerts, the euphoria of festivals, the intellectual buzz of conferences, or the adrenaline rush of sports, Quket is your passport to a lasting experience.",
+        img: "/Quiket.png",
+        img2: "/quiket2.png",
+        img3: "/quiket3.png",
+        iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+        link: "https://www.quicket.me",
+    },
+    {
+        id: 2,
+        title: "Pulzer - SaaS App",
+        des: "At Coresoft | A SAAS platform to help organizations manage their sales and inventory effectively.",
+        img: "/p2.svg",
+        iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+        link: "https://www.pulzer.io",
+    },
+    {
+        id: 3,
+        title: "AI Image SaaS - Canva Application",
+        des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
+        img: "/p3.svg",
+        iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+        link: "/ui.aiimg.com",
+    },
+    {
+        id: 4,
+        title: "Animated Apple Iphone 3D Website",
+        des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+        img: "/p4.svg",
+        iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+        link: "/ui.apple.com",
+    },
+];
+
+export const testimonials = [
+    {
+        quote:
+            "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+        name: "Mostafa Zaman Abbas",
+        title: "General Manager, IHCM-BD",
+    },
+    {
+        quote:
+            "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+        name: "Mostafa Zaman Abbas",
+        title: "General Manager, IHCM-BD",
+    },
+    {
+        quote:
+            "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+        name: "Mostafa Zaman Abbas",
+        title: "General Manager, IHCM-BD",
+    },
+    {
+        quote:
+            "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+        name: "Mostafa Zaman Abbas",
+        title: "General Manager, IHCM-BD",
+    },
+    {
+        quote:
+            "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+        name: "Mostafa Zaman Abbas",
+        title: "General Manager, IHCM-BD",
+    },
+];
+
+export const companies = [
+    {
+        id: 1,
+        name: "cloudinary",
+        img: "/cloud.svg",
+        nameImg: "/cloudName.svg",
+    },
+    {
+        id: 2,
+        name: "appwrite",
+        img: "/app.svg",
+        nameImg: "/appName.svg",
+    },
+    {
+        id: 3,
+        name: "HOSTINGER",
+        img: "/host.svg",
+        nameImg: "/hostName.svg",
+    },
+    {
+        id: 4,
+        name: "stream",
+        img: "/s.svg",
+        nameImg: "/streamName.svg",
+    },
+    {
+        id: 5,
+        name: "docker.",
+        img: "/dock.svg",
+        nameImg: "/dockerName.svg",
+    },
+];
+
+export const workExperience = [
+    {
+        id: 1,
+        title: "Fullstack Engineer - Coresoft",
+        desc: "Designed and developed SaaS and PoS web apps using Django, enhancing interactivity.",
+        className: "md:col-span-2",
+        thumbnail: "/exp1.svg",
+    },
+    {
+        id: 2,
+        title: "Student Deep Learning Researcher",
+        desc: "Assisted in the development of a deep learning pipeline using PyTorch, Keras, Pandas for stock market analysis.",
+        className: "md:col-span-2", // change to md:col-span-2
+        thumbnail: "/exp2.svg",
+    },
+];
+
+export const socialMedia = [
+    {
+        id: 1,
+        img: "/git.svg",
+        link: "https://github.com/tanimomor"
+    },
+    {
+        id: 2,
+        img: "/insta.svg",
+        link: "https://www.instagram.com/tanim.xy/"
+    },
+    {
+        id: 3,
+        img: "/link.svg",
+        link: "https://www.linkedin.com/in/tanimomor/"
+    },
+];
